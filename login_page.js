d4b1fbbc39d3334b4c58f4af62946e5cc2032a72
Coding308-Ritesha <84786471+Coding308-Ritesha@users.noreply.github.com>
2021-11-30 17:24:42 +0530

    Add files via upload

---
+++ login_page.js
@@ -0,0 +1,10 @@
+function addUser(){
+    player_1 = document.getElementById("player1_name_input").value;
+    player_2 = document.getElementById("player2_name_input").value;
+    
+    localStorage.setItem("player_1_name",player_1);
+    localStorage.setItem("player_2_name",player_2);
+    
+    window.location="quiz_game.html";
+    
+}
